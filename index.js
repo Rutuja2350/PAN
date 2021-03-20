bb86f13bb93d5d3ee19bbd7ac9570d2c6cf4737f
Rutuja2350 <65300153+Rutuja2350@users.noreply.github.com>
2021-03-20 20:05:18 +0530

    almost backened done

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express"),
+    app = express();
+    // cloudinary = require('cloudinary').v2;
+// require("dotenv").config();
+
+// cloudinary.config({ 
+//     cloud_name: 'dnmsyfrgs', 
+//     api_key: process.env.API_KEY, 
+//     api_secret: process.env.API_SECRET
+// });
+// cloudinary.uploader.upload("my_image.jpg", function(error, result) {console.log(result, error)});
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+    console.log("The PAN server started...");
+});
